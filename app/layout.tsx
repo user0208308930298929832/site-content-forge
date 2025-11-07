1164c9c56d182ff06f445a10cf803ffbce2eac02
user0208308930298929832 <miguelbretiano@gmail.com>
2025-11-07 12:10:01 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
-      <body>{children}</body>
+      <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
 }
